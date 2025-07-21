4427d25d7383b2b4cb5836a5b7e5b1351757f1b5
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-21 10:25:58 +0500

    Add alembic and configure it

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
-from core.models import db_helper
+from core.models import db_helper, Base
 from api import router as api_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
-
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield
     # shutdown
     await db_helper.dispose()
